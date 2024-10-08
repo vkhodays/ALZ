2b459a77094fa57aec9e1feab211fe3d2cb35f05
Tim Naish <tim.naish@appvia.io>
2024-10-08 20:59:51 +0100

    Bump module versions

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,7 +11,7 @@ moved {
   to   = module.global-logitics-analytics
 }
 module "global-logitics-analytics" {
-  source = "git::https://dev.azure.com/RalphLauren/Azure%20Landing%20Zones/_git/Terraform.DataLandingZone?ref=20241008.2"
+  source = "git::https://dev.azure.com/RalphLauren/Azure%20Landing%20Zones/_git/Terraform.DataLandingZone?ref=20241008.3"
 
   providers = {
     azurerm = azurerm
@@ -129,7 +129,7 @@ module "global-logitics-analytics" {
 
 module "retail-storesystems" {
   # tflint-ignore: terraform_module_pinned_source
-  source = "git::https://dev.azure.com/RalphLauren/Azure%20Landing%20Zones/_git/Terraform.LandingZones?ref=20240917.4"
+  source = "git::https://dev.azure.com/RalphLauren/Azure%20Landing%20Zones/_git/Terraform.LandingZones?ref=20241008.2"
 
   providers = {
     azurerm = azurerm

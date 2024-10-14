e3db7e888f654cfc5bb288eb9e4a2e84304f6e58
Tim Naish <tim.naish@appvia.io>
2024-10-14 19:37:35 +0100

    Test with latest DLZ module

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,7 +11,7 @@ moved {
   to   = module.global-logitics-analytics
 }
 module "global-logitics-analytics" {
-  source = "git::https://dev.azure.com/RalphLauren/Azure%20Landing%20Zones/_git/Terraform.DataLandingZone?ref=20241014.3"
+  source = "git::https://dev.azure.com/RalphLauren/Azure%20Landing%20Zones/_git/Terraform.DataLandingZone?ref=feature/access-updates"
 
   providers = {
     azurerm = azurerm
